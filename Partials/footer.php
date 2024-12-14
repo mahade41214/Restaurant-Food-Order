918b0d5db6f3ea497576a318b3e853997e2618bb
mahade41214 <mahade41214@gmail.com>
2024-12-15 02:14:32 +0600

    Restaurent PHP

---
+++ Partials/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+         <div class= "wrapper">
+                 <p class="text-center">2024 All right reserved, Restaurent Food Order Development By - <a href="#">Mahade Hassan</a></p>
+            </div>
+         </div>
+        <!-- Footer Section Ends -->
+        
+    </body>
+
+
+</html>
---
+++ manage-admin.php
@@ -0,0 +1,13 @@
+
+  <?php include('partials/menu.php');?>
+
+         <!-- Menu Content Starts -->
+         <div class="main-content">
+         <div class= "wrapper">
+                 <h1>Manage Admin</h1> 
+         </div>
+         </div>
+        <!-- Menu Content Ends -->
+<?php include('partials/footer.php');?>
+
+         
